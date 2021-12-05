280c0ce60643e0812111af351f3774db44b9d01a
Jean-Julien-Vanrossomme <jj.vanrossomme@outlook.com>
2021-12-05 16:24:04 +0100

    modif frontregister 05/12/21

--- frontregister.php
+++ frontregister.php
@@ -10,13 +10,16 @@
 <!-- faudra certainement adapter avec la table adresse -->
 <form class="box" action="./php/register.php" method="post" >
 	<h1 class="box-logo box-title"><a>Inscription</a></h1>
-   <input type="text" class="box-input" name="numero_inami" placeholder="Numéro Inami" required />
+   <input type="number" class="box-input" name="numero_inami" placeholder="Numéro Inami" required />
 	<input type="text" class="box-input" name="nom" placeholder="Nom" required  />
    <input type="text" class="box-input" name="prenom" placeholder="Prénom" required  />
    <input type="text" class="box-input" name="adresse_mail" placeholder="Adresse mail" required id="email" />
    <input type="password" class="box-input" name="password" placeholder="Mot de passe" required id="password"/>
    <input type="password" class="box-input" name="password_confirm" placeholder="Confirmation du mot de passe" required  />
-   <input type="text" class="box-input" name="numero_telephone" placeholder="Numéro de téléphone" required />
+   <input type="tel" class="box-input" name="numero_telephone" placeholder="Numéro de téléphone" required />
+   <input type="text" class="box-input" name="rue" placeholder="Rue" required />
+   <input type="text" class="box-input" name="ville" placeholder="Ville" required />
+   <input type="number" class="box-input" name="code_postal" placeholder="Code postal" required />
    <input type="submit" name="valider" class="box-button" onclick="validate()" />
    <p class="box-register">Déjà inscrit? <a href="index.php">Connectez-vous ici</a></p>
 </form>
